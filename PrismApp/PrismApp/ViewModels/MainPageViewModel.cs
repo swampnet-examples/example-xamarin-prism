2a137e5533e7e0e578b7955ce1db21b865e4a692
Pj <petejw@theswamp.co.uk>
2018-03-25 17:26:17 +0100

    ...

--- PrismApp/PrismApp/ViewModels/MainPageViewModel.cs
+++ PrismApp/PrismApp/ViewModels/MainPageViewModel.cs
@@ -3,6 +3,7 @@ using Prism.Mvvm;
 using Prism.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 
@@ -10,14 +11,23 @@ namespace PrismApp.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
-		public DelegateCommand NavigateToSpeakPageCommand { get; private set; }
-
 		public MainPageViewModel(INavigationService navigationService) 
             : base (navigationService)
         {
             Title = "Main Page";
-			NavigateToSpeakPageCommand = new DelegateCommand(NavigateToSpeakPage);
+	
+			Items.Add(new SomeItem() { Title = "One", Command = new DelegateCommand(NavigateToSpeakPage) });
+			Items.Add(new SomeItem() { Title = "Two", Command = new DelegateCommand(NavigateToSpeakPage) });
+			Items.Add(new SomeItem() { Title = "Three", Command = new DelegateCommand(NavigateToSpeakPage) });
 		}
+
+		private ObservableCollection<SomeItem> _items = new ObservableCollection<SomeItem>();
+
+		public ObservableCollection<SomeItem> Items
+		{
+			get { return _items; }
+		}
+
 
 		private async void NavigateToSpeakPage()
 		{
---
+++ PrismApp/PrismApp/ViewModels/SomeItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace PrismApp.ViewModels
+{
+    public class SomeItem
+    {
+		public string Title { get; set; }
+		public ICommand	Command { get; set; }
+	}
+}
